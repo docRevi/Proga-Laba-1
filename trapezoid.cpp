b09f8f6790376e42fc250c0cb9fc40a979c4d4d2
Revi <andrejsavchenko91@gmail.com>
2025-03-21 22:21:37 +0700

    Добавление функций для работы с трапецией

---
+++ trapezoid.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <locale>
+
+
+using namespace std;
+int calcPerimeter(int a,int b, int c, int d) {
+    return (a+b+c+d);
+}
+double calcSquare (int a, int b, int c, int d) {
+    double height = sqrt(c ^ 2 - (((a - b) ^ 2 + c ^ 2 - d ^ 2) / (2 * (a - b)) ^ 2));
+    double square = ((a+b)/2)*height;
+    return square;
+}
+double calcMidLane(int a, int b) {
+    return (a+b)/2.0;
+}
+int main() {
+
+    setlocale(LC_ALL, "");
+    vector<int> lengths(4);
+    cout << "Введите длины сторон трапеции" << endl;
+    for (int i = 0; i < 4; i++) {
+        cin >> lengths[i];
+    }
+    sort(begin(lengths), end(lengths));
+    int a = lengths[3];
+    int b = lengths[2];
+    int c = lengths[1];
+    int d = lengths[0];
+    enum menu {
+        perimeter =1,
+        square,
+        midLane
+    };
+    int choose;
+    cout << "Выберите, что хотите вычислить: \n Периметр - 1 \n Площадь - 2 \n Средняя линию - 3" << endl;
+    cin >> choose;
+    switch (choose) {
+        case menu::perimeter: {
+            int Perimeter = calcPerimeter(a, b, c, d);
+            cout << "Периметр равен: " << Perimeter << endl;
+            break;
+        }
+        case menu::square: {
+            double Square = calcSquare(a, b, c, d);
+            cout << "Площадь равна: " << Square << endl;
+            break;
+        }
+        case menu::midLane: {
+            double midLane = calcMidLane(a, b);
+            cout << "Средняя линия равна: " << midLane << endl;
+            break;
+        }
+    };
+    return 0;
+}
