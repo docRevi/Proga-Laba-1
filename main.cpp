50911f51e674fe57e39719bb7813b0aa6965614d
Revi <andrejsavchenko91@gmail.com>
2025-03-21 22:18:18 +0700

    Добавление функций для работы с трапецией

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <locale>
 #include <ostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
+
+int calcPerimeter(int a,int b, int c, int d) {
+    return (a+b+c+d);
+}
+double calcSquare (int a, int b, int c, int d) {
+    double height = sqrt(c ^ 2 - (((a - b) ^ 2 + c ^ 2 - d ^ 2) / (2 * (a - b)) ^ 2));
+    double square = ((a+b)/2)*height;
+    return square;
+}
+double calcMidLane(int a, int b) {
+    return (a+b)/2.0;
+}
 
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -16,13 +31,50 @@ int main() {
     cin >> choice;
     switch (choice) {
         case menu::trapezoid: {
+            vector<int> lengths(4);
+            cout << "Введите длины сторон трапеции" << endl;
+            for (int i = 0; i < 4; i++) {
+                cin >> lengths[i];
+            }
+            sort(begin(lengths), end(lengths));
+            int a = lengths[3];
+            int b = lengths[2];
+            int c = lengths[1];
+            int d = lengths[0];
+            enum chose {
+                perimeter =1,
+                square,
+                midLane
+            };
+            int choose;
+            cout << "Выберите, что хотите вычислить: \n Периметр - 1 \n Площадь - 2 \n Средняя линию - 3" << endl;
+            cin >> choose;
+            switch (choose) {
+                case chose::perimeter: {
+                    int Perimeter = calcPerimeter(a, b, c, d);
+                    cout << "Периметр равен: " << Perimeter << endl;
+                    break;
+                }
+                case chose::square: {
+                    double Square = calcSquare(a, b, c, d);
+                    cout << "Площадь равна: " << Square << endl;
+                    break;
+                }
+                case chose::midLane: {
+                    double midlane = calcMidLane(a, b);
+                    cout << "Средняя линия равна: " << midlane << endl;
+                    break;
+                }
+            };
             
+            break;
         }
         case menu::triangle: {
-            
+            break;
         }
         case menu::rectangle: {
-            
+            break;
         }
     }
+    return 0;
 }
